a4d36b66666006bd9d7adbca8d8673c430891d5f
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-07-24 14:49:57 -0300

    feat: initial structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Super hero app</h1>
+      <p>Simple app to list your favorites heros</p>
+    </div>
+  );
+};
+
+export default App;
