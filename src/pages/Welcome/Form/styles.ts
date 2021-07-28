92e6b9ba054da203d6fe1f94cdf0900ee7e1d058
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-07-27 21:28:09 -0300

    style: add button color

--- src/pages/Welcome/Form/styles.ts
+++ src/pages/Welcome/Form/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Form = styled.form`
+  min-width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -10,11 +11,19 @@ export const Form = styled.form`
 export const Input = styled.input`
   width: 100%;
   padding: 0.8rem 1.2rem;
+  text-transform: capitalize;
+  border-radius: 0.5rem;
+  background: rgb(245 246 250);
+  border: solid 0.1rem rgb(227 227 228);
 `;
 
 export const Button = styled.button`
   width: 100%;
   padding: 0.8rem 1.2rem;
   margin-top: 1rem;
+  border-radius: 0.5rem;
+  color: rgb(255, 255, 255);
+  background: ${({ disabled }) =>
+    disabled ? 'rgb(239 131 84 / 56%)' : 'rgb(239 131 84)'};
   cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
 `;
