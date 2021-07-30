1320846289bbb4f2a0de905ea8a3ca13a5922754
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-07-30 20:12:00 -0300

    chore: set type DefaultTheme with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import { ThemeProvider } from 'styled-components';
+
 import { AuthProvider } from 'contexts/auth';
+import { useTheme } from 'contexts/theme';
 
 import Routes from 'routes';
 
 import GlobalStyles from 'styles/global';
+import * as themes from 'styles/theme';
 
 const App = () => {
+  const { theme } = useTheme();
+
   return (
-    <AuthProvider>
-      <GlobalStyles />
-      <Routes />
-    </AuthProvider>
+    <ThemeProvider theme={themes[theme]}>
+      <AuthProvider>
+        <GlobalStyles />
+        <Routes />
+      </AuthProvider>
+    </ThemeProvider>
   );
 };
 
