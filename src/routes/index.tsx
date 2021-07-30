66ab33aab1069ab81e255539d80a3dd387f37ff3
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-07-30 19:47:20 -0300

    feat: validate private routes with signed state from auth context

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Welcome from 'pages/Welcome';
 import Dashboard from 'pages/Dashboard';
 
+import PrivateRoute from './PrivateRoute';
+
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Welcome} />
-        <Route path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/dashboard" component={Dashboard} />
       </Switch>
     </Router>
   );
